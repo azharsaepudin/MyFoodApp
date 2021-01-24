3ca63abeedae770f8cb27ceb2296e7a57f228391
azharsaepudin <azharsaepudin5@gmail.com>
2021-01-24 10:31:38 +0700

    add try catch

--- app/src/main/java/com/azhar/foodapp/MainActivity.java
+++ app/src/main/java/com/azhar/foodapp/MainActivity.java
@@ -5,7 +5,12 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -36,25 +41,34 @@ public class MainActivity extends AppCompatActivity {
 
     public void getDataFood(){
 
-        myService.getInstance().getFood().enqueue(new Callback<FoodResponse<FoodItem>>() {
-            @Override
-            public void onResponse(Call<FoodResponse<FoodItem>> call, Response<FoodResponse<FoodItem>> response) {
+        try{
+            myService.getInstance().getFood().enqueue(new Callback<FoodResponse<FoodItem>>() {
+                @Override
+                public void onResponse(Call<FoodResponse<FoodItem>> call, Response<FoodResponse<FoodItem>> response) {
 
-                FoodResponse resp = response.body();
-
-                if (resp.getData() != null && resp.getData().size() > 0){
+                    if(response.isSuccessful()) {
 
 
-                    adapter = new FoodAdapter(resp.getData(),MainActivity.this);
-                    rv_food.setAdapter(adapter);
+                        FoodResponse resp = response.body();
+
+                        if (resp.getData() != null && resp.getData().size() > 0) {
+
+                            adapter = new FoodAdapter(resp.getData(), MainActivity.this);
+                            rv_food.setAdapter(adapter);
+                        }else{
+                            Toast.makeText(MainActivity.this, resp.message, Toast.LENGTH_SHORT).show();
+                        }
+                    }
                 }
 
-            }
+                @Override
+                public void onFailure(Call<FoodResponse<FoodItem>> call, Throwable t) {
+                    Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }catch(Exception e){
+            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
 
-            @Override
-            public void onFailure(Call<FoodResponse<FoodItem>> call, Throwable t) {
-                Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 }
